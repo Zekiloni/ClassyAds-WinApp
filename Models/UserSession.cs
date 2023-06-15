43027452a7d84af50bbf52178a66a781545b8385
Zekiloni <zekirija17@gmail.com>
2023-06-15 19:18:28 +0200

    UserSession

---
+++ Models/UserSession.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyAdsWinApp.Models
+{
+    internal class UserSession
+    {
+        private static UserSession instance;
+
+        public User User { get; private set; }
+
+        public string Token { get; private set; }
+
+        private UserSession()
+        {
+        }
+
+        public static UserSession Active
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserSession();
+                }
+                return instance;
+            }
+        }
+
+        public void SetUser(User user, string token)
+        {
+            User = user;
+            Token = token;
+        }
+
+        public void ClearUser()
+        {
+            User = null;
+            Token = null;
+        }
+    }
+}
