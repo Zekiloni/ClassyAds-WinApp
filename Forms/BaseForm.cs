1057d305ad14c5866a5b0456e05da9f63a9fdb4d
Zekiloni <zekirija17@gmail.com>
2023-06-15 19:51:51 +0200

    BaseForm class

---
+++ Forms/BaseForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClassyAdsWinApp.Forms
+{
+    public partial class BaseForm : Form
+    {
+        protected Panel contentPanel;
+
+        public BaseForm()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+
+        }
+    }
+}
