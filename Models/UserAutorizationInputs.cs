d784be915823deff5473625ac762d994debf0c19
Zekiloni <zekirija17@gmail.com>
2023-06-15 19:18:54 +0200

    UserAuthorizationInputs: Login & Register

---
+++ Models/UserAutorizationInputs.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace ClassyAdsWinApp.Models
+{
+    internal class UserLoginInput
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+
+    internal class UserRegisterInput
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
