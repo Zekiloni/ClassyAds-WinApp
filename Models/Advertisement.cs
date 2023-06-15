0916997332681af38cf598098543d6755e8e0f33
Zekiloni <zekirija17@gmail.com>
2023-06-15 15:03:59 +0200

    Introduce Category & Advertisement Models

---
+++ Models/Advertisement.cs
@@ -0,0 +1,46 @@
+﻿using ClassyAdsWinApp.Enums;
+using System;
+
+
+namespace ClassyAdsWinApp.Models
+{
+    internal class Advertisement
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+
+        public bool IsNegotiable { get; set; }
+
+        public string ProductMake { get; set; }
+
+        public string ProductModel { get; set; }
+
+        public AdvertisementStatus Status { get; set; }
+
+        public bool IsFeatured { get; set; }
+
+        public bool IsPremium { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ExpiringAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyAdsWinApp.Models
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+
+        public bool IsFeatured { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual List<Advertisement> Advertisements { get; set; }
+
+        public virtual Category ParentCategory { get; set; }
+
+        public virtual List<Category> ChildCategories { get; set; }
+    }
+}
