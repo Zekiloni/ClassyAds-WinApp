8346f3392549f0841f80ce191e174e865be3b730
Zekiloni <zekirija17@gmail.com>
2023-06-15 19:51:41 +0200

    RegisterForm: BackToMain

---
+++ Forms/RegisterForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ClassyAdsWinApp.Forms
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BackToMainButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BackToMainButton
+            // 
+            this.BackToMainButton.Location = new System.Drawing.Point(100, 274);
+            this.BackToMainButton.Name = "BackToMainButton";
+            this.BackToMainButton.Size = new System.Drawing.Size(108, 33);
+            this.BackToMainButton.TabIndex = 0;
+            this.BackToMainButton.Text = "Go back";
+            this.BackToMainButton.UseVisualStyleBackColor = true;
+            this.BackToMainButton.Click += new System.EventHandler(this.BackToMainButton_Click);
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 319);
+            this.Controls.Add(this.BackToMainButton);
+            this.Name = "RegisterForm";
+            this.Text = "Register";
+            this.Load += new System.EventHandler(this.RegisterForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BackToMainButton;
+    }
+}
---
+++ Forms/RegisterForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ClassyAdsWinApp.Forms
+{
+    public partial class RegisterForm : BaseForm
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackToMainButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            MainForm mainForm = new MainForm();
+            mainForm.Show();
+        }
+    }
+}
