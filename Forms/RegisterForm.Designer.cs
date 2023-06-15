144a7d29dcb79451135dd618ceeb1b4fec06c565
Zekiloni <zekirija17@gmail.com>
2023-06-15 23:04:30 +0200

    Register form finished

--- Forms/RegisterForm.Designer.cs
+++ Forms/RegisterForm.Designer.cs
@@ -29,33 +29,192 @@
         private void InitializeComponent()
         {
             this.BackToMainButton = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.UsernameTextbox = new System.Windows.Forms.TextBox();
+            this.FirstNameTextbox = new System.Windows.Forms.TextBox();
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.LastNameTextbox = new System.Windows.Forms.TextBox();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.EmailTextbox = new System.Windows.Forms.TextBox();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.PasswordTextbox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.BirthdayLabel = new System.Windows.Forms.Label();
+            this.BirthdayDatePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // BackToMainButton
             // 
-            this.BackToMainButton.Location = new System.Drawing.Point(100, 274);
+            this.BackToMainButton.Location = new System.Drawing.Point(12, 313);
             this.BackToMainButton.Name = "BackToMainButton";
             this.BackToMainButton.Size = new System.Drawing.Size(108, 33);
-            this.BackToMainButton.TabIndex = 0;
+            this.BackToMainButton.TabIndex = 7;
             this.BackToMainButton.Text = "Go back";
             this.BackToMainButton.UseVisualStyleBackColor = true;
             this.BackToMainButton.Click += new System.EventHandler(this.BackToMainButton_Click);
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.BackColor = System.Drawing.Color.SeaGreen;
+            this.RegisterButton.ForeColor = System.Drawing.Color.White;
+            this.RegisterButton.Location = new System.Drawing.Point(210, 313);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(108, 33);
+            this.RegisterButton.TabIndex = 6;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = false;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(12, 17);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username";
+            // 
+            // UsernameTextbox
+            // 
+            this.UsernameTextbox.Location = new System.Drawing.Point(15, 37);
+            this.UsernameTextbox.MaxLength = 64;
+            this.UsernameTextbox.Name = "UsernameTextbox";
+            this.UsernameTextbox.Size = new System.Drawing.Size(132, 20);
+            this.UsernameTextbox.TabIndex = 0;
+            // 
+            // FirstNameTextbox
+            // 
+            this.FirstNameTextbox.Location = new System.Drawing.Point(15, 207);
+            this.FirstNameTextbox.MaxLength = 64;
+            this.FirstNameTextbox.Name = "FirstNameTextbox";
+            this.FirstNameTextbox.Size = new System.Drawing.Size(132, 20);
+            this.FirstNameTextbox.TabIndex = 3;
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.Location = new System.Drawing.Point(12, 187);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(55, 13);
+            this.FirstNameLabel.TabIndex = 4;
+            this.FirstNameLabel.Text = "First name";
+            // 
+            // LastNameTextbox
+            // 
+            this.LastNameTextbox.Location = new System.Drawing.Point(186, 207);
+            this.LastNameTextbox.MaxLength = 64;
+            this.LastNameTextbox.Name = "LastNameTextbox";
+            this.LastNameTextbox.Size = new System.Drawing.Size(132, 20);
+            this.LastNameTextbox.TabIndex = 4;
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.Location = new System.Drawing.Point(183, 187);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(56, 13);
+            this.LastNameLabel.TabIndex = 6;
+            this.LastNameLabel.Text = "Last name";
+            // 
+            // EmailTextbox
+            // 
+            this.EmailTextbox.Location = new System.Drawing.Point(15, 90);
+            this.EmailTextbox.MaxLength = 128;
+            this.EmailTextbox.Name = "EmailTextbox";
+            this.EmailTextbox.Size = new System.Drawing.Size(171, 20);
+            this.EmailTextbox.TabIndex = 1;
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(12, 70);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(35, 13);
+            this.EmailLabel.TabIndex = 8;
+            this.EmailLabel.Text = "E-mail";
+            // 
+            // PasswordTextbox
+            // 
+            this.PasswordTextbox.Location = new System.Drawing.Point(15, 147);
+            this.PasswordTextbox.MaxLength = 128;
+            this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.PasswordChar = '*';
+            this.PasswordTextbox.Size = new System.Drawing.Size(171, 20);
+            this.PasswordTextbox.TabIndex = 2;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 127);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 10;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // BirthdayLabel
+            // 
+            this.BirthdayLabel.AutoSize = true;
+            this.BirthdayLabel.Location = new System.Drawing.Point(12, 244);
+            this.BirthdayLabel.Name = "BirthdayLabel";
+            this.BirthdayLabel.Size = new System.Drawing.Size(45, 13);
+            this.BirthdayLabel.TabIndex = 11;
+            this.BirthdayLabel.Text = "Birthday";
+            this.BirthdayLabel.Click += new System.EventHandler(this.BirthdayLabel_Click);
+            // 
+            // BirthdayDatePicker
+            // 
+            this.BirthdayDatePicker.CustomFormat = "dd/MM/yyyy";
+            this.BirthdayDatePicker.Location = new System.Drawing.Point(15, 263);
+            this.BirthdayDatePicker.MaxDate = new System.DateTime(2023, 6, 15, 0, 0, 0, 0);
+            this.BirthdayDatePicker.MinDate = new System.DateTime(1920, 1, 1, 0, 0, 0, 0);
+            this.BirthdayDatePicker.Name = "BirthdayDatePicker";
+            this.BirthdayDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.BirthdayDatePicker.TabIndex = 5;
+            this.BirthdayDatePicker.Value = new System.DateTime(2023, 6, 15, 0, 0, 0, 0);
             // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 319);
+            this.ClientSize = new System.Drawing.Size(330, 358);
+            this.Controls.Add(this.BirthdayDatePicker);
+            this.Controls.Add(this.BirthdayLabel);
+            this.Controls.Add(this.PasswordTextbox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.EmailTextbox);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.LastNameTextbox);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.FirstNameTextbox);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Controls.Add(this.UsernameTextbox);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.RegisterButton);
             this.Controls.Add(this.BackToMainButton);
             this.Name = "RegisterForm";
             this.Text = "Register";
             this.Load += new System.EventHandler(this.RegisterForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button BackToMainButton;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.TextBox UsernameTextbox;
+        private System.Windows.Forms.TextBox FirstNameTextbox;
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.TextBox LastNameTextbox;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.TextBox EmailTextbox;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.TextBox PasswordTextbox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label BirthdayLabel;
+        private System.Windows.Forms.DateTimePicker BirthdayDatePicker;
     }
 }
--- Forms/RegisterForm.cs
+++ Forms/RegisterForm.cs
@@ -1,18 +1,25 @@
-﻿using System;
-
+﻿using ClassyAdsWinApp.Interfaces;
+using ClassyAdsWinApp.Models;
+using ClassyAdsWinApp.Services;
+using System;
+using System.Windows.Forms;
 
 namespace ClassyAdsWinApp.Forms
 {
     public partial class RegisterForm : BaseForm
     {
+        private IUserService userService;
+
+
         public RegisterForm()
         {
             InitializeComponent();
+            this.Load += RegisterForm_Load;
         }
 
         private void RegisterForm_Load(object sender, EventArgs e)
         {
-
+            userService = new UserService();
         }
 
         private void BackToMainButton_Click(object sender, EventArgs e)
@@ -20,6 +27,63 @@ namespace ClassyAdsWinApp.Forms
             this.Close();
             MainForm mainForm = new MainForm();
             mainForm.Show();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            string username = UsernameTextbox.Text;
+            string password = PasswordTextbox.Text;
+            string firstName = FirstNameTextbox.Text;
+            string lastName = LastNameTextbox.Text;
+            string email = EmailTextbox.Text;
+            DateTime birthday = BirthdayDatePicker.Value;
+
+            if (string.IsNullOrEmpty(username) )
+            {
+                MessageBox.Show("Username is required field.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(email))
+            {
+                MessageBox.Show("Email is required field.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password) )
+            {
+                MessageBox.Show("Password is required field.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty (firstName) )
+            {
+                MessageBox.Show("First name is required field.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(lastName) )
+            {
+                MessageBox.Show("Last name is required field.");
+                return;
+            }
+
+            UserRegisterInput input = new UserRegisterInput
+            {
+                Username = username,
+                Password = password,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = email,
+                DateOfBirth = birthday,
+            };
+
+            userService.Register(input);
+        }
+
+        private void BirthdayLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
